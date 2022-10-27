775c764bffd806ce534ebcf33e602c22719fc7b7
kaimo313 <kaimo313@foxmail.com>
2022-10-27 20:24:53 +0800

    feat：图片边缘模糊的效果

--- demo/common/lib/util.js
+++ demo/common/lib/util.js
@@ -130,3 +130,12 @@ export function gaussianBlur(pixels, width, height, radius = 3, sigma = radius /
     }
     return pixels;
 }
+
+export function getPixel(imageData, index) {
+    const { data } = imageData;
+    const r = data[index] / 255,
+        g = data[index + 1] / 255,
+        b = data[index + 2] / 255,
+        a = data[index + 3] / 255;
+    return [r, g, b, a];
+}
